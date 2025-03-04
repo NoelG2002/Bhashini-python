b27f349693241de0b0374b167f60a0d0473aca6c
NoelG2002 <132538006+NoelG2002@users.noreply.github.com>
2025-03-04 15:05:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ import asyncio
 from dotenv import load_dotenv
 import uvicorn
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=300)  # Increase timeout
+
 
 
 
